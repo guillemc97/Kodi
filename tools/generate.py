947e82196957d1b15771e56315ef5c9fc8a6c3e0
gcallau <55539459+gcallau@users.noreply.github.com>
2026-07-07 10:53:37 +0200

    TEST

--- tools/generate.py
+++ tools/generate.py
@@ -1,1 +1,28 @@
-print("generate addons.xml")
+import hashlib
+import os
+
+xml = '<?xml version="1.0" encoding="UTF-8"?>\n<addons>\n'
+
+for carpeta in os.listdir("."):
+
+    addon = os.path.join(carpeta, "addon.xml")
+
+    if os.path.exists(addon):
+
+        with open(addon, encoding="utf8") as f:
+
+            contenido = f.read()
+
+            contenido = contenido.split("?>")[1]
+
+            xml += contenido
+
+xml += "\n</addons>"
+
+with open("addons.xml", "w", encoding="utf8") as f:
+    f.write(xml)
+
+md5 = hashlib.md5(xml.encode()).hexdigest()
+
+with open("addons.xml.md5", "w") as f:
+    f.write(md5)
