ffecd3f45b456719799480edd707e0bdb10de07d
gcallau <55539459+gcallau@users.noreply.github.com>
2026-07-09 12:18:13 +0200

    Update fotos

--- plugin.foto.kodi/addon.py
+++ plugin.foto.kodi/addon.py
@@ -1,27 +1,35 @@
 import os
+import sys
+import xbmc
 import xbmcgui
 import xbmcplugin
-import sys
 
 handle = int(sys.argv[1])
 
 ruta = os.path.dirname(os.path.abspath(__file__))
 
+xbmc.log("Ruta del addon: " + ruta, xbmc.LOGINFO)
+
+xbmcplugin.setContent(handle, "images")
+
 for archivo in sorted(os.listdir(ruta)):
     if archivo.lower().endswith((".jpg", ".jpeg", ".png", ".webp")):
         imagen = os.path.join(ruta, archivo)
 
-        li = xbmcgui.ListItem(label=archivo)
-        li.setArt({
+        xbmc.log("Imagen encontrada: " + imagen, xbmc.LOGINFO)
+
+        item = xbmcgui.ListItem(label=archivo)
+        item.setArt({
             "thumb": imagen,
             "icon": imagen,
             "fanart": imagen
         })
+        item.setProperty("IsPlayable", "true")
 
         xbmcplugin.addDirectoryItem(
-            handle,
-            imagen,
-            li,
+            handle=handle,
+            url=imagen,
+            listitem=item,
             isFolder=False
         )
 
